fbef4cebeb36fc794954a75325696ed6a712ba79
Emanuel Gross von Trockau <egrossvontrockau@lcs.on.ca>
2023-06-01 15:02:32 -0400

    Added a Navigation Title

---
+++ Recipe Finder/Views /RecipeFinder.swift
@@ -0,0 +1,27 @@
+//
+//  RecipeFinder.swift
+//  Recipe Finder
+//
+//  Created by Emanuel Gross von Trockau on 2023-06-01.
+//
+
+import SwiftUI
+
+struct RecipeFinder: View {
+    
+
+    var body: some View {
+        Form{
+                        
+        }
+        .navigationTitle("Recipe Finder")
+    }
+}
+
+struct RecipeFinder_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            RecipeFinder()
+        }
+    }
+}
