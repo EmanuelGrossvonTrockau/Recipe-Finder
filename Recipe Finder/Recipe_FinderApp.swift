57b5e0f82ac436ea37c4e202b927c56992b5be5d
Emanuel Gross von Trockau <egrossvontrockau@lcs.on.ca>
2023-06-01 14:17:54 -0400

    Initial Commit

---
+++ Recipe Finder/Recipe_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_FinderApp.swift
+//  Recipe Finder
+//
+//  Created by Emanuel Gross von Trockau on 2023-06-01.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
